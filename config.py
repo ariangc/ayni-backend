e3bc1f7b6e726ee921fb587e520fbe7aceacc4b2
Arian <gallardo.a@pucp.pe>
2019-07-21 17:42:55 +0000

    First commit

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+
+# You need to replace the next values with the appropriate values for your configuration
+basedir = os.path.abspath(os.path.dirname(__file__))
+DEBUG = True
+PORT = 9994
+HOST = "0.0.0.0"
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER="test2", DB_PASS="froz21", DB_ADDR="127.0.0.1", DB_NAME="ayni")
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+PAGINATION_PAGE_SIZE = 5
+PAGINATION_PAGE_ARGUMENT_NAME = 'page'
